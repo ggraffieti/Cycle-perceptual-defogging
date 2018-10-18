fba391cbbf994b554132f665f4411dffae1686fa
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-10-19 01:12:30 +0200

    use of vgg16 for implement cycle-dehaze with perceptual loss

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ from data import CreateDataLoader
 from models import create_model
 from util.visualizer import save_images
 from util import html
+import torchvision
+from torchvision import models
+import torch
+import time
+from torch import nn
 
 
 if __name__ == '__main__':
@@ -18,6 +23,20 @@ if __name__ == '__main__':
     dataset = data_loader.load_data()
     model = create_model(opt)
     model.setup(opt)
+
+
+    vgg16 = models.vgg16(pretrained=True)
+
+    vgg16pool5 = vgg16.features
+    features = list(vgg16.features.children())[:-21]  # Remove last layer
+    vgg16pool2 = nn.Sequential(*features)  # Replace the model classifier
+
+    for param in vgg16pool5.parameters():
+        param.require_grad = False
+    for param in vgg16pool2.parameters():
+        param.require_grad = False
+
+
     # create a website
     web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.epoch))
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
@@ -29,8 +48,20 @@ if __name__ == '__main__':
     for i, data in enumerate(dataset):
         if i >= opt.num_test:
             break
+
+        start_time = time.time()
+        pool2 = vgg16pool2(data['A'])
+        print(time.time() - start_time)
+        start_time = time.time()
+        pool5 = vgg16pool5(data['A'])
+        print(time.time() - start_time)
+        print("pool2 ", pool2.size())
+        print("pool5 ", pool5.size())
         model.set_input(data)
+        print(i, data['A_paths'])
+        start_time = time.time()
         model.test()
+        print(time.time() - start_time)
         visuals = model.get_current_visuals()
         img_path = model.get_image_paths()
         if i % 5 == 0:
